1734aa47befd1beb450847cec8e6ec63dd3c5eb1
Nelkit Chavez <nelkitisael792@gmail.com>
2020-07-12 12:34:39 -0600

    Initial Commit

---
+++ NKAlert/NKAlert.h
@@ -0,0 +1,19 @@
+//
+//  NKAlert.h
+//  NKAlert
+//
+//  Created by Nelkit Chavez on 7/12/20.
+//  Copyright © 2020 Nelkit Chavez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NKAlert.
+FOUNDATION_EXPORT double NKAlertVersionNumber;
+
+//! Project version string for NKAlert.
+FOUNDATION_EXPORT const unsigned char NKAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKAlert/PublicHeader.h>
+
+
